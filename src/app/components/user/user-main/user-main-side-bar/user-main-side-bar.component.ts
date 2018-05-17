832280d982e5a50478bab35f20311a8d6538aeff
at-nghitran <nghi.tran@asiantech.vn>
2018-05-17 13:41:42 +0700

    create component folder

---
+++ src/app/components/user/user-main/user-main-side-bar/user-main-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-main-side-bar',
+  templateUrl: './user-main-side-bar.component.html',
+  styleUrls: ['./user-main-side-bar.component.scss']
+})
+export class UserMainSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
