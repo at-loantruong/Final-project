65e5cb202785aa10bbc19dcadb88e5be8230a5af
at-nghitran <nghi.tran@asiantech.vn>
2018-05-31 10:22:33 +0700

    update connect to APi

--- src/app/shared/services/soccer.service.ts
+++ src/app/shared/services/soccer.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
- 
- @Injectable()
- export class SoccersService {
 
-  constructor (private http: HttpClient) {
+@Injectable()
+export class SoccersService {
+
+  herderOptions: {};
+  urlApi: string;
+
+  constructor(private http: HttpClient) {
+    this.urlApi = 'http://localhost:8000/api/';
+    this.herderOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hcGkvdXNlci9sb2dpbiIsImlhdCI6MTUyNzczNTI2NiwiZXhwIjoxNTI3NzM4ODY2LCJuYmYiOjE1Mjc3MzUyNjYsImp0aSI6InRUeEpHOHhQNG1yY0RWd0EifQ.maRIY1zGOL0KS8FRY1IDqDpVQbTgZcHVYxvWZGHFHqk',
+      })
+    };
   }
 
-  public getList() {
-    return this.http.get('http://localhost:8000/api/soccers');
+  public getList(endPoint) {
+    return this.http.get(this.urlApi + endPoint, this.herderOptions);
   }
 
- }
+}
